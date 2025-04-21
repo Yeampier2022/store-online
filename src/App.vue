8a3f9440fe5cce40db3a5c18256245f5fd533aa0
Yeampier2022 <yeampierjesusa@gmail.com>
2025-04-21 19:51:37 -0400

    start project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="counter.increment()" class="bg-blue-500 text-white p-2 rounded">
+    Count: {{ counter.count }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+  
+import { useCounterStore } from '@/stores/counters'
+const counter = useCounterStore()
+
+</script>
